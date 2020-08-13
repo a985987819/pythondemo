d71683ae6950a3ffeb71891f4ceaaa9859894e6f
laoduan <985987819@qq.com>
2020-08-13 17:23:42 +0800

    'bs4和requests爬取下厨房首页高清图片'

--- demo1.py
+++ demo1.py
@@ -1,4 +1,5 @@
 import requests
+from lxml import etree
 
 proxies = {
     'https': 'https://127.0.0.1:1080',
@@ -11,10 +12,30 @@ proxies = {
 # headers = {'User-agent':ua}
 
 
+
+
+
+
 r = requests.get('https://twitter.com/LynnChe93915556',proxies = proxies)
 
+data = r.text
+# print(data)
+html = etree.HTML(data)
+# html_data = html.xpath('//span[@class="css-901oao css-16my406 r-1qd0xha r-ad9z0x r-bcqeeo r-qvutc0"]/text()')
+# html_data = html.xpath('//span[@class="css-901oao css-16my406 r-1re7ezh r-4qtqp9 r-1qd0xha r-ad9z0x r-zso239 r-bcqeeo r-qvutc0"]/text()')
+result = etree.tostring(html)
+
+# html_data = html.xpath('/html/body/div/div/div/div[2]/main/div/div/div/div[1]/div/div[2]/div/div/div[1]/div/div[3]/div/div[1]/span')
+# html_data = html.xpath('//*[@id="react-root"]/div')
+# html_data = html.xpath('/html/body/div/div/div/div[2]/div/div/div/div[1]/div/div[2]/div/div/div[1]/div/div[3]/div/div[1]/span')
+html_data = html.xpath('//span[@class="css-901oao css-16my406 r-1qd0xha r-ad9z0x r-bcqeeo r-qvutc0"]')
+
+
+
+# print(result.decode('utf-8'))
+print(html_data)
 print(r.status_code, r.reason)
-print(r.text)
+
 
 
 # r = requests.post('http://www.baidu.com',data = {'a:''1'})
---
+++ xiachufang/requests_bs4.py
@@ -0,0 +1,52 @@
+import os
+
+from bs4 import BeautifulSoup
+import requests
+from urllib.parse import urlparse
+
+
+#模拟浏览器访问
+ua = 'Mozilla/5.0 ' \
+     '(Windows NT 10.0; Win64; x64) ' \
+     'AppleWebKit/537.36 (KHTML, like Gecko) ' \
+     'Chrome/80.0.3987.116 Safari/537.36'
+header = {'User-Agent': ua}
+
+
+
+r = requests.get('http://www.xiachufang.com/',headers = header)
+
+soup = BeautifulSoup(r.text,'lxml')
+soup.select('img')
+# print(soup.select('img'))
+
+img_list = []
+for img in soup.select('img'):
+    if img.has_attr('data-src'):
+        img_list.append(img.attrs['data-src'])
+    else:
+        img_list.append(img.attrs['src'])
+
+image_dir = os.path.join(os.curdir,'images')
+
+
+for img in img_list:
+    o = urlparse(img)
+    filename = o.path[1:].split('@')[0]
+    filepath = os.path.join(image_dir,filename)
+
+# 如无目录则创建
+    if not os.path.isdir(os.path.dirname(filepath)):
+        os.mkdir(os.path.dirname(filepath))
+
+    url = '%s://%s/%s' %(o.scheme,o.netloc,filename)
+
+    # print(filepath)
+    # print(filename)
+
+    print(url)
+
+    resp = requests.get(url)
+    with open(filepath,'wb') as f:
+        for chunk in resp.iter_content(1024):
+            f.write(chunk)
